bf65eaac394fcf915a6c39e9c4a92ea0fd68e05f
mdupays <malvina.dupays@ign.fr>
2026-04-16 10:57:14 +0200

    add new function : add trajectory

---
+++ scripts/example_lauch_pretreatment.sh
@@ -0,0 +1,6 @@
+# For lauching fPCpretrreatment
+python -m lidar_for_fuel.main_pretreatment \
+  io.input_dir=./data/pointcloud/ \
+  io.input_trajectory_dir=./data/trajectory/ \
+  io.input_filename=test_semis_2024_0751_6690_LA93_IGN69_filter_trajectory_1311.laz  \
+  io.output_dir=./tmp/
